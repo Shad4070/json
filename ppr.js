7bc66d95cc9dad703c4372bcbc1adfe68c0b7db1
Shad4070 <oscar4070@hotmail.com>
2017-09-29 12:46:27 -0500

    Funcionalidad para nombres con espacio

--- ppr.js
+++ ppr.js
@@ -1,10 +1,15 @@
 var archivo=process.argv[2];
+var reem="";
+for(var z=0;z<archivo.length;z++){
+		reem+=(archivo.charAt(z)=="-")?" ":archivo.charAt(z);
+}
 var fs=require("fs");
 var mos=fs.readFileSync("status.json","utf-8");
 var ip=JSON.parse(mos);
 for(var i=0;i<ip.stations.length;i++){
 	for(var j=0;j<ip.stations[i].items.length;j++){
-		if (archivo==ip.stations[i].items[j].name.toLowerCase()) {
+
+		if (reem==ip.stations[i].items[j].name.toLowerCase()) {
 			console.log(ip.stations[i].items[j]);
 		}
 	}
